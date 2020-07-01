f40d9f88583886150032f79bd35ec8a73a157497
Jonaq Niveer Sarma <jonaqpetla@gmail.com>
2020-07-01 07:13:25 +0530

    Added XML and JSON basics into networks.py

--- network.py
+++ network.py
@@ -51,4 +51,93 @@ for tag in tags:
 # ctx.verify_mode = ssl.CERT_NONE
 # html = urllib.request.urlopen(url, context=ctx).read()
 
-# XML & JSON ===========================================================================================
+# XML and JSON
+# XML ==================================================================================================
+# ^ built for data sharing among application processes. Uses wire protocol (really? serialize - deserialize)
+
+# XML: start tag, end tag, attributes, text content. Seft closing tags in the absence of text content
+# e.g. <person height=170>Jonaq</person>. May contain tags content - think of it like a tree
+# XML can have only one outer tag. Document specs are exempt from this rule, 
+# but they have to be at the very beginning, and not after a \n for example
+
+import xml.etree.ElementTree as ET 
+
+input = '''<?xml version="1.0" encoding="utf-8" ?>
+<stuff>
+    <users>
+        <user x="2">
+            <id>001</id>
+            <name>Chuck</name>
+        </user>
+        <user x="7">
+            <id>009</id>
+            <name>Brent</name>
+        </user>
+    </users>
+</stuff>
+'''
+
+tree = ET.fromstring(input)
+
+# getting the first matches
+print('Name:', tree.find('users/user/name').text)       # getting content
+print('Attr:', tree.find('users/user').get('x'))        # getting attributes
+
+# getting everything
+lst = tree.findall('users/user')
+print('\nUser count: ', len(lst))
+
+for item in lst:
+    print('Name: ', item.find('name').text)
+    print('id: ', item.find('id').text)
+    print('Attribute: ', item.get('x'))
+
+print()
+
+# XML schema Validation can be done using pyxsd
+# xsd is the most commonly used language for xml schema specification
+# a valid schema of our example xml would be:
+sch = '''<?xml version="1.0" encoding="utf-8" ?>
+<xs:complexType name='stuff'>
+    <xs:complexType name='users'>
+        <xs:sequence>
+        <xs:complexType name='user' minOccurs='1' maxOccurs='10'>
+            <xs:sequence>
+            <xs:element name='id' type='xs:integer'/>
+            <xs:element name='id' type='xs:string'/>
+            </xs:sequence>
+        </xs:complexType>
+        </xs:sequence> 
+    </xs:complexType>
+</xs:complexType>
+'''
+
+# JSON - Javascript Object Notation ====================================================================
+# json is better for just pulling data out of a system. not as cumbersome as xml's hierarchical structure
+# json is not internationally standardized, it's just that people like it and use it
+# "key" : "value" pairs, can have objects as value
+
+import json
+data = '''
+{
+    "name" : "Chuck",
+    "phone" : 
+    {
+        "type" : "intl",
+        "number" : "+1 734 303 4456"
+    },
+    "email" : 
+    {
+        "hide" : "yes"
+    }
+}
+'''
+
+info = json.loads(data)                 # load from string, returns a dictionary
+print('Name: ', info["name"])
+print('Hide: ', info["email"]["hide"])
+
+# json might have a list of multiple dictionaries, then info will also be a list of dictionaries
+# json may not be able to represent data as complex as xml, but dealing with lists and dicts are simpler
+
+# WEB Services =========================================================================================
